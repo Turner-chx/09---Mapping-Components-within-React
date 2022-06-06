986beee5848fdac2f52f84d12e23106288f364ec
thomas <turner.thomas.dev@gmail.com>
2022-06-06 16:26:26 +0200

    zedfze

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import Contact from './components/Contact';
 
 import './style.css';
 
-// 1. populate the following array with the correct data
-const contactInfo = [];
+const contactInfo = [
+  { name : "Emmanuel", email : "emmanuel@email.com", phone : "552312354", isDeletable : true},
+  { name : "Johana",email :"johana@email.com", phone : "92034902",isDeletable : false},
+  { name : "Lucas",email :"lucas@email.com", phone :"234983499",isDeletable : true},
+  { name : "Marie",email :"marie@email.com", phone :"238942384",isDeletable : false},
+  { name : "Pedro",email :"pedro@email.com", phone :"934584875",isDeletable : true}
+];
 
 export default function App() {
   return (
@@ -12,37 +17,16 @@ export default function App() {
       <h1>My contacts list 📱</h1>
 
       <div id="container">
-        {/* 2. map contactInfo array here and display <Contact /> for each entry in the array*/}
-        <Contact
-          name="Emmanuel"
-          email="emmanuel@email.com"
-          phone="552312354"
-          isDeletable={true}
-        />
-        <Contact
-          name="Johana"
-          email="johana@email.com"
-          phone="92034902"
-          isDeletable={false}
-        />
-        <Contact
-          name="Lucas"
-          email="lucas@email.com"
-          phone="234983499"
-          isDeletable={true}
-        />
-        <Contact
-          name="Marie"
-          email="marie@email.com"
-          phone="238942384"
-          isDeletable={false}
-        />
-        <Contact
-          name="Pedro"
-          email="pedro@email.com"
-          phone="934584875"
-          isDeletable={true}
-        />
+        {
+          contactInfo.map((contactInfo,index) => (
+            <Contact 
+              name={contactInfo.name}
+              email={contactInfo.email}
+              phone={contactInfo.phone}
+              isDeletable={contactInfo.isDeletable}
+            />
+          ))
+        }
       </div>
     </div>
   );
